bb6449dea13718c8c179ac28de7e0be86f320f0e
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2022-08-12 15:34:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,17 @@ const monthcheck = (text) =>{
 
 app.use(cors())
 
+  if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const csvwriter = (originalname) =>{
     pdf(fs.readFileSync('public/' + originalname)).then(function(data) {
         const records = []
@@ -138,4 +149,10 @@ app.post('/delete',function(req, res) {
         }
     }, 1000);
 });
-app.listen(process.env.PORT, '0.0.0.0');
+
+app.listen(process.env.PORT || 5000, function() {
+    console.log("Server started.......");
+});
+
+
+
