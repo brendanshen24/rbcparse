d0ac7990b2eeeb831441bee82e490888ee4ed35c
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2022-08-12 15:11:06 -0700

    Update server.js

--- server.js
+++ server.js
@@ -139,6 +139,6 @@ app.post('/delete',function(req, res) {
     }, 1000);
 });
 
-app.listen(8778, function() {
+app.listen(8778,"rbc-pdf-to-csv.herokuapp.com", function() {
     console.log('App running on port 8000');
 });
